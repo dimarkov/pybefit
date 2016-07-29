aecb7a472725e911642864842e825791ec95aa69
dimarkov <oramale@gmail.com>
2016-07-29 17:58:33 +0200

    Fixed few errors

--- environments.py
+++ environments.py
@@ -18,8 +18,7 @@ class Environment(object):
         T (int): Duration of the experimental block, i.e. number of observations.
         d_o (int): Dimension of the observation space.
         d_x (int): Dimension of the hidden state space.
-        data (DataFrame): Data frame containg observation and hidden state values.
-        
+
     Todo:
         * Check if abstract classes and their methods should have docstrings.
     """
@@ -37,14 +36,15 @@ class Environment(object):
             self.d_x = kwargs['d_x']
         else:
             self.d_x = 1
-        
-        self.data = pd.DataFrame()
-        
-    def generate_data(self):
-        """Generates hidden states and observations."""
-        for t in range(1, self.T+1):
-            self.update_hidden_states(t)
-            self.update_observations(t)
+    
+    @abstractmethod    
+    def get_observations(self):
+        """Returns observations."""
+        pass
+    
+    @abstractmethod
+    def get_hidden_states(self):
+        """Returns hidden states."""
     
     @abstractmethod
     def expected_performance(self, *args, **kwargs):
@@ -63,7 +63,7 @@ class Environment(object):
         pass
     
     @abstractmethod        
-    def update_observations(self, t, *args):
+    def generate_observations(self, t, *args, **kwargs):
         """Generates the stimuli (observations) in current hidden state.
         
         Args:
@@ -73,35 +73,85 @@ class Environment(object):
         pass
 
 
-class BinomialEnvironment(Environment):
-    """Environment with binary observations.
+class MultiArmedBandit(Environment):
+    """Environment containing multiple bandits.
     
-       Observation are binary values sampled from the binomial distribution with 
-       dynamic expected value :math: 'p_t'. At each time step the expectations
-       would either stay unchanged with probability :math: '1-\rho' or change 
-       to a new value (drawn from an uniform distribution) with probability 
-       :math: '\rho'.
+       Observation are binary values (reward, no-reward) sampled from the 
+       binomial distributions with dynamic expected value :math: '\vec{p}_{t,n}', 
+       where :math: 'n' denotes the chosen bandit. At each time step the 
+       reward expectations across bandits would either stay unchanged with 
+       probability :math: '1-\rho' or change to a new value (drawn from an 
+       uniform distribution over space of categorical distributions) 
+       with probability :math: '\rho'.
        
        Attributes:
            rho (double): Probability of resampling the value of expectations
-           p_t (array_like): Predifned time serries of expected values
+           p_t (array_like): Time series of expected values
+           o_t (array_like): Time series of observations
+           n_b (int): number of bandits
        
     """
     
-    def __init__(self, T, rho = 0.05, p_t = None):
-        super(BinomialEnvironment, self).__init__(T)        
+    def __init__(self, T, rho = 0.05, p_t = None, n_b = 1):
+        super(MultiArmedBandit, self).__init__(T, d_o = 1, d_x = n_b)        
         self.rho = rho #switch_probability
-
+        
         if p_t is not None:
-            self.data[r'$p_t$'] = p_t #predefined time serries of expected values
+            self.p_t = p_t #predefined time serries of expected values
         else:
-            self.data[r'$o_t$'] = [np.nan]*(self.T + 1)
-            self.data[r'$p_t$'] = [np.nan]*(self.T + 1)
-            self.data[r'$p_t$'][0] = np.random.rand()
+            self.p_t = np.zeros( (self.T+1, n_b), dtype = np.double )
+            if self.d_x == 1:
+                self.p_t[0] = np.random.rand()                
+            else:
+                self.p_t[0] = np.random.dirichlet(np.ones(self.d_x))
+        
+        self.o_t = np.zeros((self.T+1, self.d_o + 1), dtype = np.double )
+        self.o_t[0, :] = -1
 
+    def get_observations(self):
+        """Make random responses and return generated observations.
+            
+        Usefull mostly in the case of a single-armed bandit (:math: 'n_b = 1').
+        """
+        #check if hidden states should be generated
+        gen_h = np.any( self.p_t[1:].sum(axis = 1) == 0 )        
+        
+        #check if observations shoudl be generated
+        gen_o = np.any( self.o_t[1:, 1] == -1 )
+        
+        if gen_o:
+            df_obs = pd.DataFrame()
+            df_obs[r'$o_t$'] = [np.nan]*(self.T + 1)
+            df_obs[r'$r_t$'] = [np.nan]*(self.T + 1)
+            for t in range(1, self.T + 1):
+                if gen_h:
+                    self.update_hidden_states(t)
+                    
+                df_obs[r'$o_t$'][t], df_obs[r'$r_t$'][t] = self.generate_observations(t)
+                
+            return df_obs
+
+        else:
+            return pd.DataFrame(self.o_t, columns = [ r'$o_t$', r'$r_t$' ] )
+
+            
+    def get_hidden_states(self):
+        
+        #check if hidden states should be generated
+        gen = np.any( self.p_t[1:].sum(axis = 1) == 0 )
+        
+        if gen:
+            for t in range(1, self.T + 1):
+                self.update_hidden_states(t)
+        
+        if self.d_x == 1:
+            return pd.DataFrame({r'$p_t$': self.p_t[:,0]})
+        else:
+            return pd.DataFrame(self.p_t, index = [ r'$p_{t,%d}$' % i for i in range(self.d_o)] )
+            
     
     def update_hidden_states(self, t):
-        """Update the value of :math: 'p_t'.
+        """Update the value of :math: '\vec{p}_t'.
         
         Args:
             t (int): Current time step.
@@ -109,30 +159,61 @@ class BinomialEnvironment(Environment):
         """
     
         if(np.random.binomial(1, self.rho)):
-            self.data[r'$p_t$'][t] = np.random.rand()
+            if self.d_x == 1:
+                self.p_t[t,:] = np.random.rand()
+            else:
+                self.p_t[t,:] = np.random.dirichlet(np.ones(self.d_x))
         else:
-            self.data[r'$p_t$'][t] = self.data[r'$p_t$'][t-1]
+            self.p_t[t,:] = self.p_t[t-1,:]
         
-    def update_observations(self, t):
+    def generate_observations(self, t, response = None):
         """Sample an observation for the current time step. 
         
         Args:
             t (int): Current time step.
+            response (int optional): Selected bandit arm
+            
+        Returns:
+            obs (bool): Observation for the given choice.
+            choice (int): Selected arm of the bandit
             
         """
         
-        self.data[r'$o_t$'][t] = np.random.binomial(1, self.data[r'$p_t$'][t])
+        if response is None:
+            #make a random arm selection
+            choice = np.random.randint(self.d_x)
+            if self.p_t[t].sum() == 0:
+                self.update_hidden_states(t)
+            
+            obs = np.random.binomial(1, self.p_t[t, choice])
+            self.o_t[t, 0] = obs
+            self.o_t[t, 1] = choice
+        else:
+            choice = response
+            if self.p_t[t].sum() == 0:
+                self.update_hidden_states(t)
+            
+            obs = np.random.binomial(1, self.p_t[t, choice])
+            self.o_t[t, 0] = obs
+            self.o_t[t, 1] = choice
+            
+        return obs, choice
         
-    def expected_performance(self, r_p):
+    def expected_performance(self):
         """ Returns the expected agents performance for the given probability 
         of selecting one of the two possible options.
         
         Args:
-            r_p (array_like): Response probability
+            r_p (array_like optional): Response probability
+            
+        Note:
+            Not usable in the case of one-armed bandit.
         
         """
-        return np.sum( self.data[r'$p_t$']*r_p + (1- self.data[r'$p_t$'])*(1-r_p) )
-    
+        return self.o_t[1:,0].sum()/self.T
+
+
+#TODO: Implement other environments (not finished)  
 class StateEnv(Environment):
     """
     One or two dimensional environment in which state of the environment
@@ -229,16 +310,19 @@ class AR2DEnv(Environment):
             self.observations[t,:] = np.dot(self.As[am], self.observations[t-ar_ord:t, :].flatten()) \
                 + np.dot(np.linalg.cholesky(self.sigma), np.random.randn(self.d))
                 
+
 def main():
     import seaborn as sns
     sns.set(style = "white", palette="muted", color_codes=True)
     
     T = 100
     
-    be = BinomialEnvironment(T)
-    be.generate_data()
-    ax = be.data.plot(y = r'$o_t$', style = 'go')
-    ax = be.data.plot(y = r'$p_t$', style = 'k-', ax = ax)
+    env = MultiArmedBandit(T)
+    obs = env.get_observations()
+    hst = env.get_hidden_states()    
+    
+    ax = obs.plot(y = r'$o_t$', style = 'go')
+    ax = hst.plot(y = r'$p_t$', style = 'k-', ax = ax)
     ax.legend(numpoints = 1)
 
 if __name__ == "__main__":
