35b8e6b7c2b3132ac2495f19618dbf94e0b31939
dimarkov <5038100+dimarkov@users.noreply.github.com>
2023-06-13 18:43:13 +0200

    updated base class default methods

--- src/pybefit/tasks/base.py
+++ src/pybefit/tasks/base.py
@@ -13,6 +13,11 @@ class Task(object):
         self.blocks = blocks  # number of experimental blocks
         self.trials = trials  # number of trials
         self.nsub = nsub  # number of subjects
+
+    def get_offer(self, block, trial, *args, **kwargs):
+        """Define an offer for a current block, trial pair that defines a unique stimuli, that is choices for a given block trial pair"""
+        
+        return None
         
     def update_environment(self, block, trial, *args, **kwargs):
         """Generate stimuli for task's current block and trial
